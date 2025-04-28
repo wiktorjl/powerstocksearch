cd63709a81e666d3151f782b5b9dd372b3810180
Wiktor Lukasik <contact@wiktor.io>
2025-04-27 19:49:37 -0700

    Removing dead code

---
+++ sql/delete.sql
@@ -0,0 +1,7 @@
+delete from ohlc_data;
+delete from splits;
+delete from indicators;
+delete from indicator_definitions;
+delete from reversal_scan_results;
+delete from symbols_details;
+delete from symbols;
